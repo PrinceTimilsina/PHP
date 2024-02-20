67a68c533c3fd1142e5eab767addcc2102b082ea
PrinceTimilsina <142810341+PrinceTimilsina@users.noreply.github.com>
2024-02-20 21:24:54 +0545

    Add files via upload

---
+++ display.php
@@ -0,0 +1,16 @@
+<?php
+include'connectingfordisplay.php';
+$sql = "SELECT * FROM display1";
+$result = $conn->query($sql);
+if($result->num_rows > 0) {
+    while($row=$result->fetch_assoc()) {
+        echo "Title:" .$row['Title'] ."<br>";
+        echo "Content:" .$row["content"] . "<br>";
+        
+    }
+} else {
+    echo "0 results";
+}
+// Close the connection 
+$conn->close();
+?>
