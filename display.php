bfc4729ba968c0f0d5dccffacce9e2d8757fe0ce
PrinceTimilsina <142810341+PrinceTimilsina@users.noreply.github.com>
2024-03-10 07:33:40 +0545

    Update display.php

--- display.php
+++ display.php
@@ -6,7 +6,6 @@ if($result->num_rows > 0) {
     while($row=$result->fetch_assoc()) {
         echo "Title:" .$row['Title'] ."<br>";
         echo "Content:" .$row["content"] . "<br>";
-        
     }
 } else {
     echo "0 results";
